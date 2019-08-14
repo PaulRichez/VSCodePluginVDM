766f409b3e8ababa1c48dd3bad41a884af5cd977
paul.richez59 <paul.richez59@gmail.com>
2019-08-14 08:27:12 +0200

    get random VDM

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';
+import * as path from 'path';
+var request = require('request');
+var DomParser = require('dom-parser');
+
+function replaceChar(string: string) {
+	return string.replace(/\n/g, " ").replace(/&nbsp;/g, "").replace(/&#039;/g, "'").replace(/&quot;/g, '"');
+}
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vdm" is now active!');
+	let disposable = vscode.commands.registerCommand('extension.vdm', () => {
+		request('https://www.viedemerde.fr/aleatoire', function (error: any, response: { statusCode: number; }, body: any) {
+			if (body && !error && response.statusCode === 200) {
+				const newFile = vscode.Uri.parse('untitled:' + path.join('', 'vdm.js'));
+				vscode.workspace.openTextDocument(newFile).then(document => {
+					const edit = new vscode.WorkspaceEdit();
+					var parser = new DomParser();
+					var dom = parser.parseFromString(body);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+					const allVdm = dom.getElementsByClassName('panel panel-classic');
+					let content = '';
+					allVdm.forEach((vdm: any) => {
+						content += '//' + replaceChar(vdm.getElementsByClassName('article-topbar')[0].textContent) + '\n';
+						content += replaceChar(vdm.getElementsByClassName('article-link')[0].textContent) + '\n';
+					});
+					edit.insert(newFile, new vscode.Position(0, 0), content);
+					return vscode.workspace.applyEdit(edit).then(success => {
+						if (success) {
+							vscode.window.showTextDocument(document);
+						} else {
+							vscode.window.showInformationMessage('Error!');
+						}
+					});
+				});
+			} else {
+				vscode.window.showErrorMessage('Erreur lors de la réception des VDM!');
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
